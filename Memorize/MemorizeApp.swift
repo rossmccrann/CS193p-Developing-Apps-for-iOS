5a88a1e4d6cb195493f19e0759cab1c4d85513a4
rossmccrann <rmccrann@tcd.ie>
2023-06-19 13:04:39 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ross McCrann on 19/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
